897bbfead0cece3daecc8ebbf1eee93680869d5a
Rob Sykes <robs@users.sourceforge.net>
2016-03-04 21:30:51 +0000

    simplify by using #cmakedefine01; introduce HAVE_AVCODEC

--- msvc/soxr-config.h
+++ msvc/soxr-config.h
@@ -1,4 +1,4 @@
-/* SoX Resampler Library      Copyright (c) 2007-13 robs@users.sourceforge.net
+/* SoX Resampler Library      Copyright (c) 2007-16 robs@users.sourceforge.net
  * Licence for this file: LGPL v2.1                  See LICENCE for details. */
 
 /* N.B. Pre-configured for typical MS-Windows systems.  However, the normal
@@ -9,11 +9,12 @@
 
 #define HAVE_SINGLE_PRECISION 1
 #define HAVE_DOUBLE_PRECISION 1
-#define HAVE_AVFFT            0
-#define HAVE_SIMD             1
-#define HAVE_FENV_H           0
-#define HAVE_LRINT            0
-#define WORDS_BIGENDIAN       0
+#define HAVE_AVFFT 0
+#define HAVE_AVCODEC 0
+#define HAVE_SIMD 1
+#define HAVE_FENV_H 0
+#define HAVE_LRINT 0
+#define WORDS_BIGENDIAN 0
 
 #include <limits.h>
 
